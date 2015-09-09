c141714146cd7dd827c4b8d2daf7626eb396b60c
Austin Kincaid & Christoph Pirringer <student@epicodus.com>
2015-09-09 13:45:35 -0700

    Initial file setup

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var primeSift = function(input) {
+  
+};
+
+
+// $(document).ready(function() {
+//   $("form#palindrome").submit(function(event) {
+//     var raw_input = $("input#raw_input").val();
+//
+//     var result = "";
+//     if (palindrome(raw_input)){
+//       result = "YES";
+//     }else{
+//       result = "NO";
+//     };
+//
+//     $(".output").text(result);
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('palindrome', function() {
+  it("returns false for the words 'Hello'", function() {
+    expect(palindrome('Hello')).to.equal(false);
+  });
+});
